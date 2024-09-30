3d7183446d6bae84b95605043ca496a7ddc99f7d
Thibeau Wouters <thibeauwouters@gmail.com>
2024-09-30 08:56:39 -0700

    Trying to get the Fisher working

--- src/paper_jose/autodiff_showcase/fisher.py
+++ src/paper_jose/autodiff_showcase/fisher.py
@@ -165,7 +165,7 @@ class Fisher:
     
     def read_hessian_values(self,
                             take_log: bool = True,
-                            verbose: bool = True):
+                            verbose: bool = False):
         
         data = np.load(self.filename, allow_pickle = True)
         names = data["names"]
@@ -191,6 +191,8 @@ class Fisher:
         plt.yticks(range(n_dim), data["names"], fontsize = 24)
         plt.savefig("./figures/hessian.png", bbox_inches='tight')
         plt.close()
+        
+        return hessian_values
         
     def check_gaussianity(self, 
                           param_name: str = "E_sym",
@@ -220,6 +222,154 @@ class Fisher:
             plt.close()
             
         return param_values, log_likelihood_list
+    
+    def invert_hessian(self,
+                       hessian: np.array,
+                       verbose: bool = True):
+        
+        # from gwfast.fisherTools import CovMatr
+        import mpmath
+        
+        FisherM = hessian
+        FisherMatrixOr = copy.deepcopy(FisherM)
+    
+        reweighted=False
+        CovMatr = np.zeros(FisherM.shape)
+        cho_failed = 0
+        typeuse='float64'
+        # FisherM = FisherMatrix.astype(typeuse)
+        
+        # Check for NaN values
+        ff = mpmath.matrix(FisherM.astype(typeuse))
+        try:
+            # Conditioning of the original Fisher
+            
+            # Checks this by computing the eigenvalues of the FIM
+            E, _ = mpmath.eigh(ff)
+            E = np.array(E.tolist(), dtype=typeuse)
+            if np.any(E < 0) and verbose:
+                print('Matrix is not positive definite!')
+                print("E")
+                print(E)
+
+            cond = np.max(np.abs(E))/np.min(np.abs(E))
+            if verbose:
+                print('Condition of original matrix: %s' %cond)
+            
+            try:
+                # Normalize by the diagonal
+                ws =  mpmath.diag([ 1/mpmath.sqrt(ff[i, i]) for i in range(FisherM.shape[-2]) ])
+                FisherM_ = ws*ff*ws
+                # Conditioning of the new Fisher
+                EE, _ = mpmath.eigh(FisherM_)
+                E = np.array(EE.tolist(), dtype=typeuse)
+                cond = np.max(np.abs(E))/np.min(np.abs(E))
+                if verbose:
+                    print('Condition of the new matrix: %s' %cond)
+                reweighted=True
+            except ZeroDivisionError:
+                print('The Fisher matrix has a zero element on the diagonal. The normalization procedure will not be applied. Consider using a prior.')
+                FisherM_ = ff
+            
+            # Gi
+            invMethodIn='inv'
+            alt_method = 'svd'
+            invMethod = invMethodIn
+            if np.any(E<0):
+                if verbose:
+                    print('Matrix is not positive definite at position!')
+                if invMethodIn=='cho':
+                    cho_failed+=1
+                    invMethod=alt_method
+                    if verbose:
+                        print('Cholesky decomposition not usable. Using method %s' %invMethod)
+            elif invMethod=='cho':
+                try:
+                    # In rare cases, the choleski decomposition still fails even if the eigenvalues are positive...
+                    # likely for very small eigenvalues
+                    c = (mpmath.cholesky(FisherM_))**-1
+                    print("c is computed!")
+                except Exception as e:
+                    print(e)
+                    invMethod=alt_method
+                    print('Cholesky decomposition not usable. Eigenvalues seem ok but cholesky decomposition failed. Using method %s' %invMethod)
+                    #print('Eigenvalues: %s' %str(E))
+                    cho_failed+=1
+
+            if invMethod=='inv':
+                    cc = FisherM_**-1
+            elif invMethod=='cho':
+                    #c = cF**-1
+                    cc = c.T*c
+            elif invMethod=='svd':
+                    U, Sm, V = mpmath.svd_r(FisherM_)
+                    S = np.array(Sm.tolist(), dtype=typeuse)
+                    
+                    # TODO: check if needed?
+                    # if ((truncate) and (np.abs(cond)>condNumbMax)):
+                    #     if verbose:
+                    #         print('Truncating singular values below %s' %svals_thresh)
+                        
+                    #     maxev = np.max(np.abs(S))
+                    #     Sinv = mpmath.matrix(np.array([1/s if np.abs(s)/maxev>svals_thresh else 1/(maxev*svals_thresh) for s in S ]).astype(typeuse))
+                    #     St = mpmath.matrix(np.array([s if np.abs(s)/maxev>svals_thresh else maxev*svals_thresh for s in S ]).astype(typeuse))
+                        
+                    #     # Also copute truncated Fisher to quantify inversion error consistently
+                    #     truncFisher = U*mpmath.diag([s for s in St])*V
+                    #     truncFisher = (truncFisher+truncFisher.T)/2
+                    #     FisherMatrixOr[:, :, k] = np.array(truncFisher.tolist(), dtype=typeuse)
+                        
+                    #     if verbose:
+                    #         truncated = np.abs(S)/maxev<svals_thresh #np.array([1 if np.abs(s)/maxev>svals_thresh else 0 for s in S ]
+                    #         print('%s singular values truncated' %(truncated.sum()))
+                    # else:
+                    Sinv = mpmath.matrix(np.array([1/s for s in S ]).astype(typeuse))
+                    St = S
+                    
+                    cc=V.T*mpmath.diag([s for s in Sinv])*U.T
+                    
+            # elif invMethod=='svd_reg':
+                
+            #         U, Sm, V = mpmath.svd_r(FisherM_)
+                    
+            #         S = np.squeeze(np.array(Sm.tolist(), dtype=typeuse))
+            #         Um = np.array(U.tolist(), dtype=typeuse)
+            #         Vm = np.array(V.tolist(), dtype=typeuse)
+
+            #         kVal = sum(S > svals_thresh)
+                                                
+            #         Sinv = mpmath.matrix(np.array([1/s  for s in S ]).astype(typeuse))
+            #         cc = mpmath.matrix(Um[:, 0:kVal] @ np.diag(1. / S[0:kVal]) @ Vm[0:kVal, :])
+                                            
+                    
+            # elif invMethod=='lu':
+            #         P, L, U = mpmath.lu(FisherM_)
+            #         ll = P*L
+            #         llinv = ll**-1
+            #         uinv=U**-1
+            #         cc = uinv*llinv
+                    
+                    
+            #### END OF INVERSION METHODS
+            
+            # Enforce symmetry.
+            cc = (cc+cc.T)/2
+            
+            if reweighted:
+                # Undo the reweighting
+                CovMatr_ = ws*cc*ws
+            else:
+                CovMatr_ = cc
+
+            CovMatr =  np.array(CovMatr_.tolist(), dtype=typeuse)
+        
+        except Exception as e:
+            # Eigenvalue decomposition failed
+            print(e)
+            raise ValueError("Eigenvalue decomposition failed")
+    
+        return CovMatr
+        
     
 # TODO: remove me
 def compare_hessians():
@@ -253,12 +403,16 @@ def main():
     ### Compute the Fisher
     fisher = Fisher(NB_CSE = 0, filename = "my_hessian_values.npz")
     
-    # fisher.compute_hessian_values()
-    # fisher.read_hessian_values()
-    # compare_hessians()
+    fisher.compute_hessian_values()
+    hessian = fisher.read_hessian_values()
+    
+    # TODO: this is still breaking!
+    # ### Inversion of Fisher matrix
+    # test = fisher.invert_hessian(hessian)
+    # print(test)
     
     ### Gaussianity
-    check_all_gaussianity(fisher)
+    # check_all_gaussianity(fisher)
     
     print("DONE")
     
