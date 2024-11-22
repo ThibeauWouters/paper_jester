6752be193b89ff4d0122fa7126851f7457b5ebfc
Thibeau Wouters <thibeauwouters@gmail.com>
2024-11-22 03:10:46 -0800

    NMMA cross check benchmark code is complete

---
+++ src/paper_jose/benchmarks/benchmarker.py
@@ -0,0 +1,274 @@
+"""
+Randomly sample EOSs and solve TOV for benchmarking purposes
+"""
+
+################
+### PREAMBLE ###
+################
+
+import psutil
+p = psutil.Process()
+p.cpu_affinity([0])
+import os
+import shutil
+import time
+
+import os
+import tqdm
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.lines import Line2D
+import seaborn as sns
+from typing import Union, Callable
+from collections import defaultdict
+
+import jax
+jax.config.update("jax_enable_x64", True)
+jax.config.update('jax_platform_name', 'cpu')
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+print(jax.devices())
+
+import jax.numpy as jnp
+from jimgw.prior import UniformPrior, CombinePrior
+from jaxtyping import Array
+import joseTOV.utils as jose_utils
+
+import paper_jose.utils as utils
+import paper_jose.inference.utils_plotting as utils_plotting
+plt.rcParams.update(utils_plotting.mpl_params)
+import seaborn as sns
+
+from paper_jose.benchmarks.nmma_tov import EOS_with_CSE
+
+
+class Benchmarker:
+    
+    """A class to randomly sample and solve for some EOSs"""
+    
+    def __init__(self,
+                 prior: CombinePrior,
+                 transform: utils.MicroToMacroTransform,
+                 outdir: str = "./random_samples/",
+                 random_seed: int = 0,
+                 nb_samples: int = 2_000,
+                 mtov_threshold: float = 2.1):
+        
+        self.prior = prior
+        self.transform = transform
+        self.jax_key = jax.random.PRNGKey(random_seed)
+        self.outdir = outdir
+        self.nb_samples = nb_samples
+        self.mtov_threshold = mtov_threshold
+        
+    def initialize_walkers(self) -> dict:
+        """
+        Initialize the walker parameters in the EOS space given the random seed.
+
+        Returns:
+            dict: Dictionary of the starting parameters.
+        """
+        
+        self.jax_key, jax_subkey = jax.random.split(self.jax_key)
+        params = self.prior.sample(jax_subkey, 1)
+            
+        # This is needed, otherwise JAX will scream
+        for key, value in params.items():
+            if isinstance(value, jnp.ndarray):
+                params[key] = value.at[0].get()
+                        
+        return params
+
+    def plot_all_eos(self, 
+                     color = "blue", 
+                     use_mask = True,
+                     verbose: bool = False):
+        
+        plt.subplots(nrows = 1, ncols = 2, figsize = (12, 8))
+        files = os.listdir(self.outdir)
+        mtov_list = np.zeros(len(files))
+        r14_list = np.zeros(len(files))
+        
+        for i, file in enumerate(files):
+            data = np.load(os.path.join(self.outdir, file))
+            masses, radii, lambdas = data["masses_EOS"], data["radii_EOS"], data["Lambdas_EOS"]
+            mask = radii < 20
+            n_false = len(mask) - np.sum(mask)
+            if verbose:
+                print(f"EOS {i} has {n_false} radii > 20 km")
+            if use_mask:
+                masses, radii, lambdas = masses[mask], radii[mask], lambdas[mask]
+                
+            mtov_list[i] = np.max(masses)
+            r14_list[i] = np.interp(1.4, masses, radii)
+            
+            plt.subplot(1, 2, 1)
+            plt.plot(radii, masses, color = color, alpha = 0.1)
+            plt.xlabel(r"Radius [km]")
+            plt.ylabel(r"Mass [$M_\odot$]")
+            plt.subplot(1, 2, 2)
+            plt.plot(masses, lambdas, color = color, alpha = 0.1)
+            plt.xlabel(r"Radius [km]")
+            plt.ylabel(r"Lambda")
+            plt.yscale("log")
+            
+        plt.savefig("./figures/all_eos.png")
+        plt.close()
+        
+        # Make a histogram of MTOV
+        plt.figure(figsize = (12, 6))
+        plt.hist(mtov_list, bins = 20, color = color, density = True, histtype = "step", linewidth = 4)
+        plt.xlabel(r"$M_{\rm TOV}$ [$M_\odot$]")
+        plt.ylabel("Density")
+        plt.savefig("./figures/mtov_hist.png")
+        plt.close()
+        
+        # Make a histogram of MTOV
+        plt.figure(figsize = (12, 6))
+        plt.hist(r14_list, bins = 20, color = color, density = True, histtype = "step", linewidth = 4)
+        plt.xlabel(r"$R_{1.4}$ [km]")
+        plt.ylabel("Density")
+        plt.savefig("./figures/r14_hist.png")
+        plt.close()
+        
+
+    def random_sample(self):
+        """
+        Generate a sample from the prior, solve the TOV equations and return the results.        
+        """
+        
+        print("Generating random samples for a batch of EOS . . . ")
+        
+        files = os.listdir(self.outdir)
+        counter = len(files)
+        
+        pbar = tqdm.tqdm(range(self.nb_samples))
+        for i in pbar:
+            params = self.initialize_walkers()
+            out = self.transform.forward(params)
+            logpc, m, r, l = out["logpc_EOS"], out["masses_EOS"], out["radii_EOS"], out["Lambdas_EOS"]
+            n, p, e, h, dloge_dlogp, cs2 = out["n"], out["p"], out["e"], out["h"], out["dloge_dlogp"], out["cs2"]
+            
+            if np.any(np.isnan(m)) or np.any(np.isnan(r)) or np.any(np.isnan(l)):
+                print(f"Sample {i} has NaNs. Skipping this sample")
+                continue
+            
+            # Only save if the TOV mass is high enough
+            if jnp.max(m) > self.mtov_threshold:
+                npz_filename = os.path.join(self.outdir, f"{counter}.npz")
+                save_dict = {**params, **out}
+                np.savez(npz_filename, **save_dict)
+                counter += 1
+                
+            pbar.set_description(f"Sample: {i} MTOV: {np.max(m)} Counter: {counter}")
+            
+        return
+    
+    def make_crosscheck_plots(self, 
+                              other_dir: str,
+                              max_nb: int = 100,
+                              colors: list[str] = ["blue", "red"],
+                              other_label: str = "NMMA"):
+        
+        pbar = tqdm.tqdm(range(1, max_nb))
+        for i in pbar:
+            # Load jose result:
+            jose_filename = os.path.join(self.outdir, f"{i}.npz")
+            jose_data = np.load(jose_filename)
+            m_jose, r_jose, l_jose = jose_data["masses_EOS"], jose_data["radii_EOS"], jose_data["Lambdas_EOS"]
+            
+            # Load other result:
+            other_filename = os.path.join(other_dir, f"{i}.npz")
+            other_data = np.load(other_filename, allow_pickle=True)
+            m_other, r_other, l_other = other_data["masses_EOS"], other_data["radii_EOS"], other_data["Lambdas_EOS"]
+            
+            plt.subplots(nrows = 1, ncols = 2, figsize = (12, 6))
+            plt.subplot(1, 2, 1)
+            plt.plot(r_jose, m_jose, color = colors[0], label = "JESTER")
+            plt.plot(r_other, m_other, color = colors[1], linestyle = "--", label = other_label)
+            plt.xlabel(r"Radius [km]")
+            plt.ylabel(r"Mass [$M_\odot$]")
+            
+            plt.subplot(1, 2, 2)
+            plt.plot(m_jose, l_jose, color = colors[0])
+            plt.plot(m_other, l_other, color = colors[1], linestyle = "--", label = other_label)
+            plt.xlabel(r"Mass [$M_\odot$]")
+            plt.ylabel(r"Lambda")
+            plt.yscale("log")
+            
+            plt.legend()
+            plt.savefig(f"./figures/crosschecks/{i}.png")
+            plt.close()
+    
+    
+class CrosscheckSolver:
+    
+    def __init__(self, 
+                 jose_dir: str = "./random_samples/",
+                 outdir: str = "./nmma/"):
+    
+        self.jose_dir = jose_dir
+        self.outdir = outdir
+        
+    def load_and_solve_nmma(self, idx: int) -> None:
+        
+        """Load precomputed JESTER EOS and construct an NMMA EOS from it"""
+        
+        # Load data
+        og_filename = os.path.join(self.jose_dir, f"{idx}.npz")
+        data = np.load(og_filename)
+        n, p, e = data["n"], data["p"], data["e"]
+        logpc = data["logpc_EOS"]
+        m, r, l = data["masses_EOS"], data["radii_EOS"], data["Lambdas_EOS"]
+        
+        # Preprocess for NMMA consumption
+        low_density_eos = {"n": n / jose_utils.fm_inv3_to_geometric, 
+                           "p": p / jose_utils.MeV_fm_inv3_to_geometric,
+                           "e": e / jose_utils.MeV_fm_inv3_to_geometric}
+        
+        n_max = max(low_density_eos["n"])
+        eos_nmma = EOS_with_CSE(low_density_eos, n_lim = n_max)
+        pc = jnp.exp(logpc)
+        pc = pc / jose_utils.MeV_fm_inv3_to_geometric
+        
+        start = time.time()
+        masses_EOS, radii_EOS, Lambdas_EOS = eos_nmma.construct_family(pc)
+        end = time.time()
+        
+        np.savez(os.path.join(self.outdir, f"{idx}.npz"), timing = end-start, masses_EOS = masses_EOS, radii_EOS = radii_EOS, Lambdas_EOS = Lambdas_EOS)
+        
+    def load_and_solve_all(self,
+                           max_nb: int = 100,
+                           with_nmma: bool = True):
+        
+        pbar = tqdm.tqdm(range(1, max_nb))
+        for idx in pbar:
+            if with_nmma:
+                self.load_and_solve_nmma(idx)
+            else:
+                raise NotImplementedError("Only NMMA is implemented so far, perhaps can implement another cross check method later on as well")
+            
+    
+def main():
+    
+    ### Choose to create own prior or can also fetch the one from the utils
+    prior = utils.prior
+    transform = utils.MicroToMacroTransform(name_mapping = utils.name_mapping)
+    benchmarker = Benchmarker(prior = prior, transform = transform, nb_samples=10_000)
+    
+    ### Benchmark jose/jester:
+    # benchmarker.random_sample()
+    # benchmarker.plot_all_eos()
+    
+    ### Use another solver, e.g. NMMA
+    max_nb = 10
+    crosscheck = CrosscheckSolver()
+    crosscheck.load_and_solve_all(max_nb = max_nb)
+    
+    ### Diagnosis/cross-check plots
+    benchmarker.make_crosscheck_plots(other_dir = crosscheck.outdir,
+                                      max_nb=max_nb)
+    
+if __name__ == "__main__":
+    main()
---
+++ src/paper_jose/benchmarks/nmma_tov.py
@@ -0,0 +1,313 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+import scipy.constants
+from scipy.interpolate import CubicSpline, interp1d
+from scipy.integrate import solve_ivp, cumulative_trapezoid
+from scipy.optimize import minimize_scalar
+
+# unit conversion for pressure / energy_density
+particle_to_SI = scipy.constants.e * 1e51
+SI_to_geometric = scipy.constants.G / np.power(scipy.constants.c, 4.0)
+particle_to_geometric = particle_to_SI * SI_to_geometric
+c = 299792458.0
+G = 6.6743e-11
+Msun = 1.988409870698051e30
+solar_mass_in_meter = Msun * G / c / c # solar mass in geometric unit
+
+
+class EOS_with_CSE(object):
+    """
+    Create and eos object with an array of (n, p, e) as the
+    low-density tail. And extend the eos to higher density with
+    speed-of-sound interpolation. And with the corresponding
+    (m, r, lambda) relation solved.
+
+    Parameters:
+        low_density_eos: dict, with numpy arrays of n, p, and e in fm^-3, MeV fm^-3 and MeV fm^-3
+        n_connect: float, take the low density eos up to the given number density (default: 0.16)
+        n_lim: float, having the eos extend to the given number density (default: 2)
+        N_seg: int, number of speed-of-sound extension segments (default: 5)
+        cs2_limt: float, speed-of-sound squared limit in c^2 (default: 1)
+        seed: int, seed of random draw extension (default: 42)
+    """
+
+    def __init__(self, low_density_eos: dict, n_lim=2., seed=42):
+
+        self.seed = seed
+        self.n_lim = n_lim
+
+        self.n_array = low_density_eos['n']
+        self.p_array = low_density_eos['p']
+        self.e_array = low_density_eos['e']
+        # self.h_array = low_density_eos['h']
+        
+        self.__calculate_pseudo_enthalpy()
+        self.__construct_all_interpolation()
+
+
+    def __calculate_pseudo_enthalpy(self):
+
+        intergrand = self.p_array / (self.e_array + self.p_array)
+        self.h_array = cumulative_trapezoid(intergrand, np.log(self.p_array), initial=0) + intergrand[0]
+
+    def __construct_all_interpolation(self):
+
+        self.log_energy_density_from_log_pressure = interp1d(np.log(self.p_array),
+                                                             np.log(self.e_array), kind='linear',
+                                                             fill_value='extrapolate',
+                                                             assume_sorted=True)
+        self.log_energy_density_from_log_pseudo_enthalpy = interp1d(np.log(self.h_array),
+                                                                    np.log(self.e_array), kind='linear',
+                                                                    fill_value='extrapolate',
+                                                                    assume_sorted=True)
+        self.log_energy_density_from_log_number_density = interp1d(np.log(self.n_array),
+                                                                   np.log(self.e_array), kind='linear',
+                                                                   fill_value='extrapolate',
+                                                                   assume_sorted=True)
+
+        self.log_pressure_from_log_energy_density = interp1d(np.log(self.e_array),
+                                                             np.log(self.p_array), kind='linear',
+                                                             fill_value='extrapolate',
+                                                             assume_sorted=True)
+        self.log_pressure_from_log_number_density = interp1d(np.log(self.n_array),
+                                                             np.log(self.p_array), kind='linear',
+                                                             fill_value='extrapolate',
+                                                             assume_sorted=True)
+        self.log_pressure_from_log_pseudo_enthalpy = interp1d(np.log(self.h_array),
+                                                              np.log(self.p_array), kind='linear',
+                                                              fill_value='extrapolate',
+                                                              assume_sorted=True)
+
+        self.log_number_density_from_log_pressure = interp1d(np.log(self.p_array),
+                                                             np.log(self.n_array), kind='linear',
+                                                             fill_value='extrapolate',
+                                                             assume_sorted=True)
+        self.log_number_density_from_log_pseudo_enthalpy = interp1d(np.log(self.h_array),
+                                                                    np.log(self.n_array), kind='linear',
+                                                                    fill_value='extrapolate',
+                                                                    assume_sorted=True)
+        self.log_number_density_from_log_energy_density = interp1d(np.log(self.e_array),
+                                                                   np.log(self.n_array), kind='linear',
+                                                                   fill_value='extrapolate',
+                                                                   assume_sorted=True)
+
+        self.log_pseudo_enthalpy_from_log_pressure = interp1d(np.log(self.p_array),
+                                                              np.log(self.h_array), kind='linear',
+                                                              fill_value='extrapolate',
+                                                              assume_sorted=True)
+        self.log_pseudo_enthalpy_from_log_energy_density = interp1d(np.log(self.e_array),
+                                                                    np.log(self.h_array), kind='linear',
+                                                                    fill_value='extrapolate',
+                                                                    assume_sorted=True)
+        self.log_pseudo_enthalpy_from_log_number_density = interp1d(np.log(self.n_array),
+                                                                    np.log(self.h_array), kind='linear',
+                                                                    fill_value='extrapolate',
+                                                                    assume_sorted=True)
+
+        self.log_dedp_from_log_pressure = interp1d(np.log(self.p_array),
+                                                   np.gradient(np.log(self.e_array), np.log(self.p_array)),
+                                                   kind='linear',
+                                                   fill_value='extrapolate',
+                                                   assume_sorted=True)
+
+    def energy_density_from_pressure(self, p):
+        return np.exp(self.log_energy_density_from_log_pressure(np.log(p)))
+
+    def energy_density_from_pseudo_enthalpy(self, h):
+        return np.exp(self.log_energy_density_from_log_pseudo_enthalpy(np.log(h)))
+
+    def energy_density_from_number_density(self, n):
+        return np.exp(self.log_energy_density_from_log_number_density(np.log(n)))
+
+    def pressure_from_energy_density(self, e):
+        return np.exp(self.log_pressure_from_log_energy_density(np.log(e)))
+
+    def pressure_from_pseudo_enthalpy(self, h):
+        return np.exp(self.log_pressure_from_log_pseudo_enthalpy(np.log(h)))
+
+    def pressure_from_number_density(self, n):
+        return np.exp(self.log_pressure_from_log_number_density(np.log(n)))
+
+    def number_density_from_pressure(self, p):
+        return np.exp(self.log_number_density_from_log_pressure(np.log(p)))
+
+    def number_density_from_pseudo_enthalpy(self, h):
+        return np.exp(self.log_number_density_from_log_pseudo_enthalpy(np.log(h)))
+
+    def number_density_from_energy_density(self, e):
+        return np.exp(self.log_number_density_from_log_energy_density(np.log(e)))
+
+    def pseudo_enthalpy_from_pressure(self, p):
+        return np.exp(self.log_pseudo_enthalpy_from_log_pressure(np.log(p)))
+
+    def pseudo_enthalpy_from_number_density(self, n):
+        return np.exp(self.log_pseudo_enthalpy_from_log_number_density(np.log(n)))
+
+    def pseudo_enthalpy_from_energy_density(self, e):
+        return np.exp(self.log_pseudo_enthalpy_from_log_energy_density(np.log(e)))
+
+    def dedp_from_pressure(self, p):
+        e = self.energy_density_from_pressure(p)
+        return e / p * self.log_dedp_from_log_pressure(np.log(p))
+
+    def construct_family(self, pcs):
+
+        ndat = len(pcs)
+        pc_min = 3.5  # arbitary lower bound pc in MeV fm^-3
+        pc_max = self.pressure_from_number_density(self.n_lim * 0.999)
+        pcs = np.logspace(np.log10(pc_min), np.log10(pc_max), num=ndat)
+        
+
+        # Generate the arrays of mass, radius and k2
+        ms = []
+        rs = []
+        ks = []
+        logpcs = []
+
+        for i, pc in enumerate(pcs):
+            m, r, k2 = TOVSolver(self, pc)
+            ms.append(m)
+            rs.append(r)
+            ks.append(k2)
+            logpcs.append(np.log(pc))
+
+            if len(ms) > 1 and ms[-1] < ms[-2]:
+                break
+
+        ms = np.array(ms)
+        rs = np.array(rs)
+        ks = np.array(ks)
+
+        # if i != ndat - 1:
+        #     # build a interpolation for logpc-mass to get logpc at max mass
+        #     f = interp1d(logpcs, -ms, kind='linear')
+        #     res = minimize_scalar(f, method='bounded',
+        #                           bounds=(logpcs[0] * 1.001, logpcs[-1] * 0.999))
+        #     logpmax = res.x
+        #     pmax = np.exp(logpmax)
+        #     mmax, rmax, kmax = TOVSolver(self, pmax)
+
+        #     # replace the last entry with the actual maximum
+        #     logpcs[-1] = logpmax
+        #     ms[-1] = mmax
+        #     rs[-1] = rmax
+        #     ks[-1] = kmax
+
+        # calculate the compactness
+        cs = ms / rs
+
+        # convert the mass to solar mass
+        ms /= solar_mass_in_meter
+        # convert the radius to km
+        rs /= 1e3
+
+        # calculate the tidal deformability
+        lambdas = 2. / 3. * ks * np.power(cs, -5.)
+
+        # # build the mass-lambda interpolation
+        # lambda_m_interp = interp1d(ms, lambdas, kind='linear')
+
+        # # build the mass-radius interpolation
+        # radius_m_interp = interp1d(ms, rs, kind='linear')
+
+        return ms, rs, lambdas
+
+
+def tov_ode(h, y, eos):
+    r, m, H, b = y
+    e = eos.energy_density_from_pseudo_enthalpy(h) * particle_to_geometric
+    p = eos.pressure_from_pseudo_enthalpy(h) * particle_to_geometric
+    dedp = e / p * eos.log_dedp_from_log_pressure(np.log(p / particle_to_geometric))
+
+    A = 1.0 / (1.0 - 2.0 * m / r)
+    C1 = 2.0 / r + A * (2.0 * m / (r * r) + 4.0 * np.pi * r * (p - e))
+    C0 = A * (
+        -(2) * (2 + 1) / (r * r)
+        + 4.0 * np.pi * (e + p) * dedp
+        + 4.0 * np.pi * (5.0 * e + 9.0 * p)
+    ) - np.power(2.0 * (m + 4.0 * np.pi * r * r * r * p) / (r * (r - 2.0 * m)), 2.0)
+
+    drdh = -r * (r - 2.0 * m) / (m + 4.0 * np.pi * r * r * r * p)
+    dmdh = 4.0 * np.pi * r * r * e * drdh
+    dHdh = b * drdh
+    dbdh = -(C0 * H + C1 * b) * drdh
+
+    dydt = [drdh, dmdh, dHdh, dbdh]
+
+    return dydt
+
+
+def calc_k2(R, M, H, b):
+
+    y = R * b / H
+    C = M / R
+
+    num = (
+        (8.0 / 5.0)
+        * np.power(1 - 2 * C, 2.0)
+        * np.power(C, 5.0)
+        * (2 * C * (y - 1) - y + 2)
+    )
+    den = (
+        2
+        * C
+        * (
+            4 * (y + 1) * np.power(C, 4)
+            + (6 * y - 4) * np.power(C, 3)
+            + (26 - 22 * y) * C * C
+            + 3 * (5 * y - 8) * C
+            - 3 * y
+            + 6
+        )
+    )
+    den -= (
+        3
+        * np.power(1 - 2 * C, 2)
+        * (2 * C * (y - 1) - y + 2)
+        * np.log(1.0 / (1 - 2 * C))
+    )
+
+    return num / den
+
+
+def TOVSolver(eos, pc_pp):
+
+    # central values
+    hc = eos.pseudo_enthalpy_from_pressure(pc_pp)
+    ec = eos.energy_density_from_pressure(pc_pp) * particle_to_geometric
+    pc = pc_pp * particle_to_geometric
+    dedp_c = eos.dedp_from_pressure(pc_pp)
+    dhdp_c = 1.0 / (ec + pc)
+    dedh_c = dedp_c / dhdp_c
+
+    # initial values
+    dh = -1e-3 * hc
+    h0 = hc + dh
+    h1 = -dh
+    r0 = np.sqrt(3.0 * (-dh) / 2.0 / np.pi / (ec + 3.0 * pc))
+    r0 *= 1.0 - 0.25 * (ec - 3.0 * pc - 0.6 * dedh_c) * (-dh) / (ec + 3.0 * pc)
+    m0 = 4.0 * np.pi * ec * np.power(r0, 3.0) / 3.0
+    m0 *= 1.0 - 0.6 * dedh_c * (-dh) / ec
+    H0 = r0 * r0
+    b0 = 2.0 * r0
+
+    y0 = [r0, m0, H0, b0]
+
+    sol = solve_ivp(tov_ode, (h0, h1), y0, args=(eos,), rtol=1e-3, atol=0.0)
+
+    # take one final Euler step to get to the surface
+    R = sol.y[0, -1]
+    M = sol.y[1, -1]
+    H = sol.y[2, -1]
+    b = sol.y[3, -1]
+
+    y1 = [R, M, H, b]
+    dydt1 = tov_ode(h1, y1, eos)
+
+    for i in range(0, len(y1)):
+        y1[i] += dydt1[i] * (0.0 - h1)
+
+    R, M, H, b = y1
+    k2 = calc_k2(R, M, H, b)
+
+    return M, R, k2
