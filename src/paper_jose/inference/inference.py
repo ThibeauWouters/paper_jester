35e50d9c84aa60025789ac8826014ca536c096fc
ThibeauWouters <thibeauwouters@gmail.com>
2024-12-02 15:28:02 +0100

    Ran inference but some broken EOS and not sure if any information is in here?

--- src/paper_jose/inference/inference.py
+++ src/paper_jose/inference/inference.py
@@ -38,12 +38,67 @@ import utils_plotting
 
 start = time.time()
 
+#############
+### PRIOR ###
+#############
+
+USE_CSE = True
+
+my_nbreak = 2.0 * 0.16
+NMAX_NSAT = 25
+NMAX = NMAX_NSAT * 0.16
+NB_CSE = 8
+width = (NMAX - my_nbreak) / (NB_CSE + 1)
+
+### NEP priors
+K_sat_prior = UniformPrior(150.0, 300.0, parameter_names=["K_sat"])
+Q_sat_prior = UniformPrior(-500.0, 1100.0, parameter_names=["Q_sat"])
+Z_sat_prior = UniformPrior(-2500.0, 1500.0, parameter_names=["Z_sat"])
+
+E_sym_prior = UniformPrior(28.0, 45.0, parameter_names=["E_sym"])
+L_sym_prior = UniformPrior(10.0, 200.0, parameter_names=["L_sym"])
+K_sym_prior = UniformPrior(-300.0, 100.0, parameter_names=["K_sym"])
+Q_sym_prior = UniformPrior(-800.0, 800.0, parameter_names=["Q_sym"])
+Z_sym_prior = UniformPrior(-2500.0, 1500.0, parameter_names=["Z_sym"])
+
+prior_list = [
+    E_sym_prior,
+    L_sym_prior, 
+    K_sym_prior,
+    Q_sym_prior,
+    Z_sym_prior,
+
+    K_sat_prior,
+    Q_sat_prior,
+    Z_sat_prior,
+]
+
+### CSE priors
+if USE_CSE:
+    nbreak_prior = UniformPrior(1.0 * 0.16, 2.0 * 0.16, parameter_names=[f"nbreak"])
+    prior_list.append(nbreak_prior)
+    for i in range(NB_CSE):
+        left = 2.1 * 0.16
+        right = 24.0 * 0.16
+        prior_list.append(UniformPrior(left, right, parameter_names=[f"n_CSE_{i}"]))
+        prior_list.append(UniformPrior(0.0, 1.0, parameter_names=[f"cs2_CSE_{i}"]))
+
+    # Final point to end
+    prior_list.append(UniformPrior(0.0, 1.0, parameter_names=[f"cs2_CSE_{NB_CSE}"]))
+
+prior = CombinePrior(prior_list)
+sampled_param_names = prior.parameter_names
+print("sampled_param_names")
+print(sampled_param_names)
+name_mapping = (sampled_param_names, ["masses_EOS", "radii_EOS", "Lambdas_EOS"])
+name_mapping_full_EOS = (sampled_param_names, ["logpc_EOS", "masses_EOS", "radii_EOS", "Lambdas_EOS", "n", "p", "h", "e", "dloge_dlogp", "cs2"])
+
 ##################
 ### LIKELIHOOD ###
 ##################
 
 # Likelihood: choose which PSR(s) to perform inference on:
-GW_id = "real"
+GW_id = "injection"
 print(f"Loading GW data from {GW_id}")
 likelihoods_list_GW = [utils.GWlikelihood(GW_id)]
 
@@ -58,11 +113,18 @@ if len(REX_names) > 0:
     print(f"Loading PREX/CREX from {REX_names}")
 likelihoods_list_REX = [utils.REXLikelihood(rex) for rex in REX_names]
 
-my_transform = utils.MicroToMacroTransform(utils.name_mapping,
+my_transform = utils.MicroToMacroTransform(name_mapping,
                                            keep_names = ["E_sym", "L_sym"],
-                                           nmax_nsat = utils.NMAX_NSAT,
-                                           nb_CSE = utils.NB_CSE,
+                                           nmax_nsat = NMAX_NSAT,
+                                           nb_CSE = NB_CSE,
                                            )
+
+# Define a separate transform which also outputs the full EOS besides the NS, for postprocessing stuff
+my_transform_full_EOS = utils.MicroToMacroTransform(name_mapping_full_EOS,
+                                                    keep_names = ["E_sym", "L_sym"],
+                                                    nmax_nsat = NMAX_NSAT,
+                                                    nb_CSE = NB_CSE,
+                                                    )
 
 likelihoods_list = likelihoods_list_GW + likelihoods_list_NICER + likelihoods_list_REX
 likelihood = utils.CombinedLikelihood(likelihoods_list)
@@ -73,18 +135,9 @@ likelihood = utils.CombinedLikelihood(likelihoods_list)
 
 
 # Sampler kwargs
-mass_matrix = jnp.eye(utils.prior.n_dim)
+mass_matrix = jnp.eye(prior.n_dim)
 local_sampler_arg = {"step_size": mass_matrix * 1e-2}
-test_kwargs = {"n_loop_training": 2,
-          "n_loop_production": 2,
-          "n_chains": 10,
-          "n_local_steps": 2,
-          "n_global_steps": 5,
-          "n_epochs": 5,
-          "train_thinning": 1,
-          "output_thinning": 1,
-}
-production_kwargs = {"n_loop_training": 10,
+kwargs = {"n_loop_training": 10,
           "n_loop_production": 10,
           "n_chains": 500,
           "n_local_steps": 2,
@@ -94,15 +147,13 @@ production_kwargs = {"n_loop_training": 10,
           "output_thinning": 1,
 }
 
-kwargs = production_kwargs
-
 jim = Jim(likelihood,
-          utils.prior,
+          prior,
           local_sampler_arg = local_sampler_arg,
           likelihood_transforms = [my_transform],
           **kwargs)
 
-jim.sample(jax.random.PRNGKey(1))
+jim.sample(jax.random.PRNGKey(2))
 jim.print_summary()
 
 ##############
@@ -150,7 +201,7 @@ max_idx = np.argmax(log_prob)
 max_log_prob = log_prob[max_idx]
 max_values = {k: v[max_idx] for k, v in named_values.items()}
 
-transformed_max_log_prob = my_transform.forward(max_values)
+transformed_max_log_prob = my_transform_full_EOS.forward(max_values)
 np.savez(os.path.join(outdir, "max_log_prob.npz"), max_values=max_values, transformed_max_log_prob=transformed_max_log_prob)
 
 # Sample a few EOS from the posterior samples:
