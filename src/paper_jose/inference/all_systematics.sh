02c8aaf1d64ba5931adbf337463423deb207c44c
ThibeauWouters <thibeauwouters@gmail.com>
2025-03-04 16:46:18 +0100

    For now, run N_CSE systematics/scalings from 10 onwards (bug in low N_CSE runs)

--- src/paper_jose/inference/all_systematics.sh
+++ src/paper_jose/inference/all_systematics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Loop over each GW event ID
-for NB_CSE in 2 5 10 20 30 40 50; do
+for NB_CSE in 10 20 30 40 50; do
   SH_FILE="./CSE_systematics/outdir_${NB_CSE}/submit.sh"
 
   # Submit the job to SLURM
